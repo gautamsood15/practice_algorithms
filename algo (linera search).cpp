01793673f57d400cd94a484505e73f661f83a0cb
gautam sood <gauty22@gmail.com>
2017-12-29 21:14:08 +0530

    Add files via upload

---
+++ algo (linera search).cpp
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+#include<time.h>
+int main(){
+	int n;
+	int a[n];
+	printf("enter the number of elements in the array \n");
+	scanf("%d",&n);
+	printf("enter the elements of the arrray");
+	for(int i = 0;i<n;i++){
+		scanf("%d",&a[i]);
+	}
+	int x,c;
+	printf("enter the element to be searched");
+	scanf("%d",&x);
+	
+	for(int i=0;i<n;i++)
+	if(a[i] = x){
+		c = i;
+		}
+  printf("the number is at index %d",c);
+  
+ return 0; 
+}
